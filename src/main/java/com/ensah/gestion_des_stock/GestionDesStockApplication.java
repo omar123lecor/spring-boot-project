c921f4b11b3ce45af66765a7bc0dff2243e393d3
omar <omarsabor21@gmail.com>
2025-05-04 14:04:25 +0100

    Initial commit

---
+++ src/main/java/com/ensah/gestion_des_stock/GestionDesStockApplication.java
@@ -0,0 +1,13 @@
+package com.ensah.gestion_des_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDesStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ensah/gestion_des_stock/GestionDesStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensah.gestion_des_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
