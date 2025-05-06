d9bbdce7edda41108fed8671d57afe7adc905a9e
omar <omarsabor21@gmail.com>
2025-05-06 22:43:29 +0100

    class produit prêt

--- src/main/java/com/ensah/gestion_des_stock/Dao/Produit.java
+++ src/main/java/com/ensah/gestion_des_stock/Dao/Produit.java
@@ -1,7 +1,49 @@
 package com.ensah.gestion_des_stock.Dao;
 
+import jakarta.persistence.*;
+
 /**
  * @author $ {USER}
  **/
+@MappedSuperclass
 public class Produit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nom;
+    private String unite;
+    private int qte;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getUnite() {
+        return unite;
+    }
+
+    public void setUnite(String unite) {
+        this.unite = unite;
+    }
+
+    public int getQte() {
+        return qte;
+    }
+
+    public void setQte(int qte) {
+        this.qte = qte;
+    }
 }
